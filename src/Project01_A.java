4b0461792ff5e678f2613410c5e7deba6ca33363
bluedog129 <bluedog129@gmail.com>
2021-06-21 23:16:27 +0900

    Project01_A 추가 (JSON API활용)

---
+++ src/Project01_A.java
@@ -0,0 +1,37 @@
+import java.util.ArrayList;
+import java.util.List;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import Kr.inflearn.BookDTO;
+
+public class Project01_A {
+
+	public static void main(String[] args) {
+		//JSON(BookDTO) -> JSON(String)
+		BookDTO dto = new BookDTO("자바", 21000, "에이콘", 670);
+		Gson g = new Gson();
+		String json = g.toJson(dto);
+		System.out.println(json);  //{"title":"자바","price":21000,"company":"에이콘","page":670}
+		
+		//JSON(String) -> Object(BookDTO)
+		BookDTO dto1 = g.fromJson(json, BookDTO.class);
+		System.out.println(dto1.getTitle()+"\t"+dto1.getPrice()); //BookDTO [title=자바, price=21000, company=에이콘, page=670]
+		
+		// object(List<BookDTO>) -> JSON(String) : [{  }, {  }...]
+		List<BookDTO> lst = new ArrayList<BookDTO>();
+		lst.add(new BookDTO("자바1", 21000, "에이콘1", 570));
+		lst.add(new BookDTO("자바2", 31000, "에이콘2", 270));
+		lst.add(new BookDTO("자바3", 11000, "에이콘3", 470));
+		
+		String lstJson = g.toJson(lst);
+		System.out.println(lstJson);
+		
+		// JSON(String) -> object(List<BookDTO>)
+		List<BookDTO> lst1= g.fromJson(lstJson, new TypeToken<List<BookDTO>>() {}.getType());
+		for(BookDTO vo : lst1) {
+			System.out.println(vo);
+		}
+	}
+}
